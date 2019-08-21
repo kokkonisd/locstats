af909a155325422b5562fe387d4a1bd5209b97fa
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-08-21 12:50:03 +0200

    Fix tests

--- locstats/tests/test_loc.py
+++ locstats/tests/test_loc.py
@@ -11,7 +11,7 @@ class TestLOC(unittest.TestCase):
         self.maxDiff = None
 
         self.assertEqual(
-            sorted(get_source_files(BASE_DIR, '.py')),
+            sorted(get_source_files(BASE_DIR, '.py', True)),
             sorted([
                 os.path.join(BASE_DIR, 'definitions.py'),
                 os.path.join(BASE_DIR, 'loc.py'),
@@ -23,7 +23,9 @@ class TestLOC(unittest.TestCase):
         )
 
         self.assertEqual(
-            sorted(get_source_files(os.path.join(BASE_DIR, 'tests'), '.py')),
+            sorted(get_source_files(os.path.join(BASE_DIR, 'tests',),
+                                    '.py',
+                                    True)),
             sorted([
                 os.path.join(BASE_DIR, 'tests', 'test_loc.py'),
                 os.path.join(BASE_DIR, 'tests', 'test_definitions.py'),
