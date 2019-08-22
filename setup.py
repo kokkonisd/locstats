a51d42923a32bc4b1be82fc944a5e51c2e61d2ec
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-08-22 12:37:30 +0200

    Change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "locstats",
-    version = "1.0.0",
+    version = "1.0.1",
     author = "Dimitri Kokkonis",
     author_email = "kokkonisd@gmail.com",
     description = "A statistics tool for your LOC per language",
