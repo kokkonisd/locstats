b1dc92f82fd6c5131d6e356221bcead2de31d3c8
Dimitris Kokkonis <kokkonisd@gmail.com>
2020-02-22 17:13:21 +0200

    Fix indent in definitions.py, add missing space, add missing ''' python comment format

--- locstats/definitions.py
+++ locstats/definitions.py
@@ -14,7 +14,7 @@ VERSION = "1.0.1"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LANG_FILE = os.path.join(BASE_DIR, "languages.json")
 with open(LANG_FILE, "r") as ext:
-        LANG_DATA = json.loads(ext.read())
+    LANG_DATA = json.loads(ext.read())
 
 
 def info(message, ending = '\n'):
